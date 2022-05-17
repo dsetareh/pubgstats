29cb8b291ec57b3db376ea86566795a2a4373fb4
dsetareh <dsetareh@vt.edu>
2022-05-17 01:33:11 -0400

    plots

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 ##### 649 Games (Mar 27 2022 - May 16 2022)
 |Map|Image| 0-5 | 6-10 | 11+ |
 | :-: | :-: | :-: | :--: | :-: |
-| **Miramar<br>(192 games)** | <img src="img/Miramar_EN.webp" alt="drawing" width="250"/> | 66.15% | 22.4% | 11.46% |
-| **Erangel (Remastered)<br>(162 games)** | <img src="img/Pubg_erangel_new.jpg" alt="drawing" width="250"/> | 61.73% | 17.28% | 20.99% |
-| **Taego<br>(155 games)** | <img src="img/taego.jpg" alt="drawing" width="250"/> | 67.74% | 17.42% | 14.84% |
-| **Karakin<br>(48 games)** | <img src="img/Karakin_Map.webp" alt="drawing" width="250"/> | 87.5% | 10.42% | 2.08% |
-| **Sanhok<br>(45 games)** | <img src="img/Sanhok-map.webp" alt="drawing" width="250"/> | 80.0% | 15.56% | 4.44% |
-| **Haven<br>(35 games)** | <img src="img/Heaven_Minimap.webp" alt="drawing" width="250"/> | 71.43% | 22.86% | 5.71% |
-| **Vikendi<br>(12 games)** | <img src="img/Vikendi_Map.webp" alt="drawing" width="250"/> | 83.33% | 0.0% | 16.67% |
+| **Miramar<br>(192 games)** | <img src="img/Miramar_EN.webp" width="250"/> | 66.15% | 22.4% | 11.46% |
+| **Erangel (Remastered)<br>(162 games)** | <img src="img/Pubg_erangel_new.jpg" width="250"/> | 61.73% | 17.28% | 20.99% |
+| **Taego<br>(155 games)** | <img src="img/taego.jpg" width="250"/> | 67.74% | 17.42% | 14.84% |
+| **Karakin<br>(48 games)** | <img src="img/Karakin_Map.webp" width="250"/> | 87.5% | 10.42% | 2.08% |
+| **Sanhok<br>(45 games)** | <img src="img/Sanhok-map.webp" width="250"/> | 80.0% | 15.56% | 4.44% |
+| **Haven<br>(35 games)** | <img src="img/Heaven_Minimap.webp" width="250"/> | 71.43% | 22.86% | 5.71% |
+| **Vikendi<br>(12 games)** | <img src="img/Vikendi_Map.webp" width="250"/> | 83.33% | 0.0% | 16.67% |
+<img src="data/killsPerWeekday.png" width="350"/> <img src="data/killsPerHour.png" width="350"/>
--- babagstats.py
+++ babagstats.py
@@ -1,18 +1,65 @@
 import json, requests, time, argparse
 from datetime import datetime
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 # args
 parser = argparse.ArgumentParser(description='get forsen babag stats')
-parser.add_argument('--datasource', type=str, required=False, help='pull from [file, api] (default: file)', default='file')
-parser.add_argument('--showStats', type=bool, required=False, help='output stats during runtime (default: True)', default=True)
-parser.add_argument('--genReadme', type=bool, required=False, help='generate readme (default: True)', default=True)
-parser.add_argument('--earliestyear', type=int, required=False, help='earliest year cutoff (default: 2021)', default=2021)
-parser.add_argument('--earliestmonth', type=int, required=False, help='earliest month cutoff (default: 1)', default=1)
-parser.add_argument('--numgamesonmap', type=int, required=False, help='num games needed on a map to put in readme (default: 10)', default=10)
-parser.add_argument('--requesttimeout', type=float, required=False, help='api request timeout (default: 0.1)', default=0.1)
+parser.add_argument('--datasource',
+                    type=str,
+                    required=False,
+                    help='pull from [file, api] (default: file)',
+                    default='file')
+parser.add_argument('--showStats',
+                    type=bool,
+                    required=False,
+                    help='output stats during runtime (default: True)',
+                    default=True)
+parser.add_argument('--genReadme',
+                    type=bool,
+                    required=False,
+                    help='generate readme (default: False)',
+                    default=False)
+parser.add_argument('--genKillsPerHour',
+                    type=bool,
+                    required=False,
+                    help='generate KillsPerHour (default: False)',
+                    default=False)
+parser.add_argument('--genKillsPerWeekday',
+                    type=bool,
+                    required=False,
+                    help='generate KillsPerWeekday (default: False)',
+                    default=False)
+parser.add_argument('--earliestyear',
+                    type=int,
+                    required=False,
+                    help='earliest year cutoff (default: 2021)',
+                    default=2021)
+parser.add_argument('--earliestmonth',
+                    type=int,
+                    required=False,
+                    help='earliest month cutoff (default: 1)',
+                    default=1)
+parser.add_argument('--dpi',
+                    type=int,
+                    required=False,
+                    help='plot dpi (default: 600)',
+                    default=600)
+parser.add_argument(
+    '--numgamesonmap',
+    type=int,
+    required=False,
+    help='num games needed on a map to put in readme (default: 10)',
+    default=10)
+parser.add_argument('--requesttimeout',
+                    type=float,
+                    required=False,
+                    help='api request timeout (default: 0.1)',
+                    default=0.1)
 args = parser.parse_args()
 
-
+plot_dpi = args.dpi
 date_cutoff = datetime(args.earliestyear, args.earliestmonth, 1)
 num_games_on_map_cutoff = args.numgamesonmap
 
@@ -21,30 +68,30 @@ requestTimeout = args.requesttimeout
 date_fmt = '%Y-%m-%dT%H:%M:%S+0000'
 
 mapNameTranslate = {
-  "Baltic_Main": "Erangel (Remastered)",
-  "Chimera_Main": "Paramo",
-  "Desert_Main": "Miramar",
-  "DihorOtok_Main": "Vikendi",
-  "Erangel_Main": "Erangel",
-  "Heaven_Main": "Haven",
-  "Range_Main": "Camp Jackal",
-  "Savage_Main": "Sanhok",
-  "Summerland_Main": "Karakin",
-  "Tiger_Main": "Taego"
+    "Baltic_Main": "Erangel (Remastered)",
+    "Chimera_Main": "Paramo",
+    "Desert_Main": "Miramar",
+    "DihorOtok_Main": "Vikendi",
+    "Erangel_Main": "Erangel",
+    "Heaven_Main": "Haven",
+    "Range_Main": "Camp Jackal",
+    "Savage_Main": "Sanhok",
+    "Summerland_Main": "Karakin",
+    "Tiger_Main": "Taego"
 }
 
 mapImgTranslate = {
     "Baltic_Main": "Pubg_erangel_new.jpg",
-    "Chimera_Main": "", # dont think this is used anymore
+    "Chimera_Main": "",  # dont think this is used anymore
     "Desert_Main": "Miramar_EN.webp",
     "DihorOtok_Main": "Vikendi_Map.webp",
-    "Erangel_Main": "Pubg_erangel_new.jpg", # looks the same 2 me
+    "Erangel_Main": "Pubg_erangel_new.jpg",  # looks the same 2 me
     "Heaven_Main": "Heaven_Minimap.webp",
-    "Range_Main": "", # tutorial map
+    "Range_Main": "",  # tutorial map
     "Savage_Main": "Sanhok-map.webp",
     "Summerland_Main": "Karakin_Map.webp",
     "Tiger_Main": "taego.jpg"
-  }
+}
 
 
 def pullLatestStats():
@@ -52,21 +99,25 @@ def pullLatestStats():
     url = 'https://pubg.op.gg/api/users/59fe352b55aa60000188a0fb/matches/recent?queue_size=1&mode=fpp&type=official'
     after = '&after='
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
+        'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
     }
-    all_games = json.loads(requests.get(url, headers=headers).content)['matches']['items']
+    all_games = json.loads(requests.get(
+        url, headers=headers).content)['matches']['items']
     num_matches = len(all_games)
-    last_match_offset = all_games[-1]['offset'] # base64 encoded str
+    last_match_offset = all_games[-1]['offset']  # base64 encoded str
 
     while num_matches == 20:
-        response = json.loads(requests.get(url + after + last_match_offset, headers=headers).content)['matches']['items']
+        response = json.loads(
+            requests.get(url + after + last_match_offset,
+                         headers=headers).content)['matches']['items']
         all_games.extend(response)
         num_matches = len(response)
         last_match_offset = response[-1]['offset']
         print("Pulled " + str(num_matches) + " matches")
-        time.sleep(requestTimeout) # just in case
+        time.sleep(requestTimeout)  # just in case
 
-    with open('fullGameData.json', 'w') as outfile:
+    with open('data/fullGameData.json', 'w') as outfile:
         json.dump(all_games, outfile)
     print("============================================================")
 
@@ -90,14 +141,122 @@ def getKillsPerMap(gameList):
     return killsPerMap
 
 
+def getKillsPerWeekday(gameList):
+    killsPerWeekday = {
+        "Monday": [],
+        "Tuesday": [],
+        "Wednesday": [],
+        "Thursday": [],
+        "Friday": [],
+        "Saturday": [],
+        "Sunday": []
+    }
+    for game in gameList:
+        curr_num_kills = game['participant']['stats']['combat']['kda']['kills']
+        curr_game_kills_type = -1
+        if curr_num_kills <= 5:
+            curr_game_kills_type = 0
+        elif curr_num_kills <= 10:
+            curr_game_kills_type = 1
+        else:
+            curr_game_kills_type = 2
+        killsPerWeekday[datetime.strptime(
+            game['started_at'],
+            date_fmt).strftime('%A')].append(curr_game_kills_type)
+    return killsPerWeekday
+
+
+def plotKillsPerWeekday(killsPerWeekday):
+    print(" == Plotting Kills Per Weekday == ")
+    labels = [
+        "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",
+        "Sunday"
+    ]
+    type0games = list(
+        map(lambda x: round(killsPerWeekday[x].count(0) / len(killsPerWeekday[x]), 2) * 100, killsPerWeekday))
+    type1games = list(
+        map(lambda x: round(killsPerWeekday[x].count(1) / len(killsPerWeekday[x]), 2) * 100, killsPerWeekday))
+    type2games = list(
+        map(lambda x: round(killsPerWeekday[x].count(2) / len(killsPerWeekday[x]), 2) * 100, killsPerWeekday))
+    x = np.arange(len(labels))
+    width = 0.20
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width * 1.5, type0games, width, label='0-5')
+    rects2 = ax.bar(x - width / 2, type1games, width, label='6-10')
+    rects3 = ax.bar(x + width / 2, type2games, width, label='11+')
+    ax.set_ylabel('Percentage of Games')
+    ax.set_title('Average # of Kills/Game by Weekday')
+    ax.set_xticks(x, labels)
+    ax.legend()
+    ax.bar_label(rects1, padding=3)
+    ax.bar_label(rects2, padding=3)
+    ax.bar_label(rects3, padding=3)
+    fig.tight_layout()
+    plt.savefig('data/killsPerWeekday.png', dpi=plot_dpi)
+    print("============================================================")
+
+
+def getKillsPerHour(gameList):
+    killsPerHour = [[] for _ in range(24)]
+    for game in gameData:
+        curr_game_date = datetime.strptime(game['started_at'], date_fmt)
+        curr_game_hour = curr_game_date.hour
+        curr_num_kills = game['participant']['stats']['combat']['kda']['kills']
+        curr_game_kills_type = -1
+        if curr_num_kills <= 5:
+            curr_game_kills_type = 0
+        elif curr_num_kills <= 10:
+            curr_game_kills_type = 1
+        else:
+            curr_game_kills_type = 2
+        killsPerHour[curr_game_hour].append(curr_game_kills_type)
+    return killsPerHour[18:23]  # streaming hours
+
+
+def plotKillsPerHour(killsPerHour):
+    print(" == Plotting Kills Per Hour == ")
+    labels = ['Hour 1', 'Hour 2', 'Hour 3', 'Hour 4', 'Hour 5']
+    type0games = list(
+        map(lambda x: round(x.count(0) / len(x), 2) * 100, killsPerHour))
+    type1games = list(
+        map(lambda x: round(x.count(1) / len(x), 2) * 100, killsPerHour))
+    type2games = list(
+        map(lambda x: round(x.count(2) / len(x), 2) * 100, killsPerHour))
+    x = np.arange(len(labels))
+    width = 0.20
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width * 1.5, type0games, width, label='0-5')
+    rects2 = ax.bar(x - width / 2, type1games, width, label='6-10')
+    rects3 = ax.bar(x + width / 2, type2games, width, label='11+')
+    ax.set_ylabel('Percentage of Games')
+    ax.set_title('Average # of Kills/Game by Stream Hour')
+    ax.set_xticks(x, labels)
+    ax.legend()
+    ax.bar_label(rects1, padding=3)
+    ax.bar_label(rects2, padding=3)
+    ax.bar_label(rects3, padding=3)
+    fig.tight_layout()
+    plt.savefig('data/killsPerHour.png', dpi=plot_dpi)
+    print("============================================================")
+
+
+
 def genReadme(gameList):
     killsPerMap = getKillsPerMap(gameList)
     print(" == Generating Readme == ")
     readme = open("README.md", "w")
     readme.write("# Forsen BabaG Stats\n")
-    readme.write("##### " + str(len(gameList)) + " Games (" + datetime.strptime(getOldestGame(gameList)['started_at'], date_fmt).strftime('%b %d %Y') + " - " + datetime.strptime(getNewestGame(gameList)['started_at'], date_fmt).strftime('%b %d %Y') + ")\n")
-    readme.write("|Map|Image| 0-5 | 6-10 | 11+ |\n| :-: | :-: | :-: | :--: | :-: |\n")
-    for gameMap in sorted(killsPerMap, key=lambda k: len(killsPerMap[k]), reverse=True):
+    readme.write(
+        "##### " + str(len(gameList)) + " Games (" +
+        datetime.strptime(getOldestGame(gameList)['started_at'],
+                          date_fmt).strftime('%b %d %Y') + " - " +
+        datetime.strptime(getNewestGame(gameList)['started_at'],
+                          date_fmt).strftime('%b %d %Y') + ")\n")
+    readme.write(
+        "|Map|Image| 0-5 | 6-10 | 11+ |\n| :-: | :-: | :-: | :--: | :-: |\n")
+    for gameMap in sorted(killsPerMap,
+                          key=lambda k: len(killsPerMap[k]),
+                          reverse=True):
         gameMapData = killsPerMap[gameMap]
         if (len(gameMapData) < num_games_on_map_cutoff):
             continue
@@ -111,24 +270,40 @@ def genReadme(gameList):
                 SixToTen += 1
             else:
                 ElevenPlus += 1
-        readme.write("| **" + mapNameTranslate[gameMap] + "<br>(" + str(len(gameMapData)) + " games)** | <img src=\"img/" + mapImgTranslate[gameMap] + "\" alt=\"drawing\" width=\"250\"/> | " + str(round(zeroToFive / len(gameMapData) * 100, 2)) + "% | " + str(round(SixToTen / len(gameMapData) * 100, 2)) + "% | " + str(round(ElevenPlus / len(gameMapData) * 100, 2)) + "% |\n")
+        readme.write("| **" + mapNameTranslate[gameMap] + "<br>(" +
+                     str(len(gameMapData)) + " games)** | <img src=\"img/" +
+                     mapImgTranslate[gameMap] +
+                     "\" width=\"250\"/> | " +
+                     str(round(zeroToFive / len(gameMapData) * 100, 2)) +
+                     "% | " +
+                     str(round(SixToTen / len(gameMapData) * 100, 2)) +
+                     "% | " +
+                     str(round(ElevenPlus / len(gameMapData) * 100, 2)) +
+                     "% |\n")
+    readme.write("<img src=\"data/killsPerWeekday.png\" width=\"350\"/> <img src=\"data/killsPerHour.png\" width=\"350\"/>\n")
     readme.close()
     print("============================================================")
+    plotKillsPerWeekday(getKillsPerWeekday(gameData))
+    plotKillsPerHour(getKillsPerHour(gameData))
 
 
 def getNewestGame(gameList):
     currNewest = gameList[0]
     for game in gameList:
-        if datetime.strptime(game['started_at'], date_fmt) > datetime.strptime(currNewest['started_at'], date_fmt):
+        if datetime.strptime(game['started_at'], date_fmt) > datetime.strptime(
+                currNewest['started_at'], date_fmt):
             currNewest = game
     return currNewest
+
 
 def getOldestGame(gameList):
     currOldest = gameList[0]
     for game in gameList:
-        if datetime.strptime(game['started_at'], date_fmt) < datetime.strptime(currOldest['started_at'], date_fmt):
+        if datetime.strptime(game['started_at'], date_fmt) < datetime.strptime(
+                currOldest['started_at'], date_fmt):
             currOldest = game
     return currOldest
+
 
 def filterGames(gameList):
     num_games_cutoff_date = 0
@@ -154,34 +329,38 @@ def filterGames(gameList):
     print("============================================================")
     return filteredGames
 
+
 def loadSavedGameData():
     try:
-        with open('fullGameData.json', 'r') as infile:
+        with open('data/fullGameData.json', 'r') as infile:
             return json.load(infile)
     except:
-        print("Error opening fullGameData.json, use --datasource=api to pull new data")
+        print(
+            "Error opening data/fullGameData.json, use --datasource=api to pull new data"
+        )
         exit()
-        
+
+
 def printStats(gameList):
     killsPerMap = getKillsPerMap(gameList)
     print(" == Stats == ")
     print("Total games: ", len(gameList))
-    print("Oldest game: ", datetime.strptime(getOldestGame(gameList)['started_at'], date_fmt).strftime('%b %d %Y'))
-    print("Newest game: ", datetime.strptime(getNewestGame(gameList)['started_at'], date_fmt).strftime('%b %d %Y'))
+    print(
+        "Oldest game: ",
+        datetime.strptime(getOldestGame(gameList)['started_at'],
+                          date_fmt).strftime('%b %d %Y'))
+    print(
+        "Newest game: ",
+        datetime.strptime(getNewestGame(gameList)['started_at'],
+                          date_fmt).strftime('%b %d %Y'))
     print("============================================================")
-
-
-
 
 
 print("=====================Forsen BabaG Stats=====================")
 print("Date Cutoff: " + date_cutoff.strftime('%b %d %Y'))
 print("Number of games on map cutoff: " + str(num_games_on_map_cutoff))
-print("")
 print("Data Source: " + args.datasource)
-print("Generating readme: " + str(args.genReadme))
 print("============================================================")
-
 
 gameData = []
 if (args.datasource == 'file'):
@@ -200,3 +379,11 @@ if (args.showStats):
 
 if (args.genReadme):
     genReadme(gameData)
+    args.genKillsPerHour = False
+    args.genKillsPerWeekday = False
+
+if (args.genKillsPerHour):
+    plotKillsPerHour(getKillsPerHour(gameData))
+
+if (args.genKillsPerWeekday):
+    plotKillsPerWeekday(getKillsPerWeekday(gameData))
